7d622dde262f9333c77c951276af7056f2de62cb
ajmainwasif01 <nipunazmun@gmail.com>
2024-10-25 17:48:10 +0600

    greatest number

---
+++ greatest_number.py
@@ -0,0 +1,14 @@
+a = int(input("Enter the first number:"))
+b = int(input("Enter the second number:"))
+c = int(input("Enter the third number:"))
+
+if a > b :
+    if a > c:
+     print("A will win")
+    else:
+     print("B will win")
+else:
+    if b > c:
+      print("B will win")
+    else:
+      print("C will win")
